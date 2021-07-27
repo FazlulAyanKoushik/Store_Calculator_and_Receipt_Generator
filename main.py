cd11a2137c60fb4c3b4e04852db4a7106fae4d93
FazlulAyanKoushik <ak.fazlulayan117@gmail.com>
2021-07-28 01:10:36 +0600

    Store calculator done

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Write a python program which will keep adding stream of numbers inputted by User.
+The adding stops as soon as user presses q key on the keyboard"""
+
+sum = 0
+
+while True:
+    userInput = input("Enter the item price or press 'q' to quit: ")
+    if userInput != 'q':
+        sum = sum + int(userInput)
+        print(f"Your order total so far {sum}")
+    else:
+        print(f"Your total bill is {sum}. Thanks for coming")
+        break
